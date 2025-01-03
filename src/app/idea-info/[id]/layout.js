a782294fc4db5cd28a6137fe9f1a5b72499d0ba9
Smarika Ghimire <smarikaghimire35@gmail.com>
2025-01-03 21:51:25 +0545

    feat: fixed the navbar ui in deatiled idea page

--- src/app/idea-info/[id]/layout.js
+++ src/app/idea-info/[id]/layout.js
@@ -2,9 +2,9 @@ import React from "react";
 import Navbar from "@/components/navigation/Navbar";
 export default function DetailedIdea({ children }) {
   return (
-    <>
+    <div className="relative min-h-screen bg-gradient-to-br bg-black text-white overflow-hidden">
       <Navbar />
       <div className="">{children}</div>
-    </>
+    </div>
   );
 }
--- src/app/idea-info/[id]/page.js
+++ src/app/idea-info/[id]/page.js
@@ -12,12 +12,12 @@ import {
   Users,
   Link,
   Download,
-  ChevronRight,
 } from "lucide-react";
+
 
 const IdeaDetailsPage = () => {
   if (!ideaData) return <p>Loading...</p>;
-  // Sample data - replace with your actual data
+
   const ideaData = {
     id: "1234",
     address: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
