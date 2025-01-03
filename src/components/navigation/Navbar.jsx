83aac8150a35a6444bdf418b502cbadd97cfd866
Jiniyas Shah <jiniyasshahts@gmail.com>
2025-01-03 20:49:33 +0545

    feat: add Radix UI progress and card components, and update Navbar styles

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+
 const Navbar = () => {
   return (
     <header className="relative container mx-auto px-4 py-8 z-10">
       <nav className="flex justify-between items-center">
-        {/* The component is used to navigate to landing page. */}
         <Link href="/" className="flex items-center gap-2">
           <Image
             src="/icons/A-logo.png"
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className="flex flex-row items-center space-x-4">
           <Link
             href="#features"
-            className="hover:text-blue-400 transition-colors"
+            className="hover:text-blue-400 transition-colors text-sm md:text-base"
           >
             Features
           </Link>
@@ -35,7 +35,6 @@ const Navbar = () => {
               href="/sign-up"
               className={buttonVariants({
                 size: "sm",
-
                 className:
                   "hidden md:flex bg-[#034ae2] text-white hover:bg-[#0452fb]",
               })}
