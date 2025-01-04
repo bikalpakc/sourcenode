be16f616fa36aba5ff438e00dbecba9de1c6d9d7
Jiniyas Shah <jiniyasshahts@gmail.com>
2025-01-04 06:46:25 +0545

    feat: Enhance Idea Submission Layout with Navbar and user authentication

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -3,10 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import ConnectWallet from "./ConnectWallet";
-const Navbar = ({ showConnectWallet }) => {
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+const Navbar = async ({ showConnectWallet = false }) => {
+  const user = await currentUser();
+
   return (
     <header className="relative container mx-auto px-4 py-8 z-10">
       <nav className="flex justify-between items-center">
+        {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <Image
             src="/icons/A-logo.png"
@@ -17,32 +23,40 @@ const Navbar = ({ showConnectWallet }) => {
           />
           <span className="text-xl font-medium">SourceNode</span>
         </Link>
-        <div className="flex flex-row items-center space-x-4">
-          <Link
-            href="#features"
-            className="hover:text-blue-400 transition-colors text-sm md:text-base"
-          >
-            Features
-          </Link>
-          <div className="flex items-center gap-4">
-            <Link
-              href="/sign-in"
-              className={buttonVariants({ size: "sm", variant: "ghost" })}
-            >
-              Login
-            </Link>
-            <Link
-              href="/sign-up"
-              className={buttonVariants({
-                size: "sm",
-                className:
-                  "hidden md:flex bg-[#034ae2] text-white hover:bg-[#0452fb]",
-              })}
-            >
-              Sign Up
-            </Link>
-            {showConnectWallet && <ConnectWallet />}
-          </div>
+
+        {/* Navigation Links */}
+        <div className="flex items-center gap-4">
+          {!user ? (
+            <>
+              <Link
+                href="#features"
+                className="hover:text-blue-400 transition-colors text-sm md:text-base"
+              >
+                Features
+              </Link>
+              <Link
+                href="/sign-in"
+                className={buttonVariants({ size: "sm", variant: "ghost" })}
+              >
+                Login
+              </Link>
+              <Link
+                href="/sign-up"
+                className={buttonVariants({
+                  size: "sm",
+                  className:
+                    "hidden md:flex bg-[#034ae2] text-white hover:bg-[#0452fb]",
+                })}
+              >
+                Sign Up
+              </Link>
+            </>
+          ) : (
+            <>
+              <UserButton />
+              {showConnectWallet && <ConnectWallet />}
+            </>
+          )}
         </div>
       </nav>
     </header>
